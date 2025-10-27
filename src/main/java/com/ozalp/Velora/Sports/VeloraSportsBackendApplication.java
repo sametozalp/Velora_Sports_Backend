632ddf6ee23c328bddb30abc79e29aa4d1c13265
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2025-10-27 21:56:54 +0300

    Initial commit

---
+++ src/main/java/com/ozalp/Velora/Sports/VeloraSportsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ozalp.Velora.Sports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeloraSportsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeloraSportsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ozalp/Velora/Sports/VeloraSportsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ozalp.Velora.Sports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeloraSportsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
