1e5e9f2de17752585e33fb5fc9eb54420cdda1f8
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2025-10-29 15:34:47 +0300

    update create membership func

--- src/main/java/com/ozalp/Velora/Sports/business/concretes/MembershipManager.java
+++ src/main/java/com/ozalp/Velora/Sports/business/concretes/MembershipManager.java
@@ -1,6 +1,8 @@
 package com.ozalp.Velora.Sports.business.concretes;
 
 import com.ozalp.Velora.Sports.business.abstracts.MembershipService;
+import com.ozalp.Velora.Sports.business.abstracts.OrganizationService;
+import com.ozalp.Velora.Sports.business.abstracts.UserService;
 import com.ozalp.Velora.Sports.business.dtos.requests.CreateMembershipRequest;
 import com.ozalp.Velora.Sports.business.dtos.responses.CreateMembershipResponse;
 import com.ozalp.Velora.Sports.business.mappers.MembershipMapper;
@@ -20,6 +22,8 @@ public class MembershipManager implements MembershipService {
 
     private final MembershipRepository repository;
     private final MembershipMapper mapper;
+    private final OrganizationService organizationService;
+    private final UserService userService;
 
     @Override
     public Membership create(Membership membership) {
@@ -36,6 +40,8 @@ public class MembershipManager implements MembershipService {
     public CreateMembershipResponse create(CreateMembershipRequest request) {
         Membership membership = mapper.toEntity(request);
         membership.setMembershipStatus(MembershipStatus.ACTIVE);
+        membership.setOrganization(organizationService.findById(UUID.fromString(request.getOrganizationId())));
+        membership.setUser(userService.findById(UUID.fromString(request.getUserId())));
         return mapper.toResponse(repository.save(membership));
     }
 }
