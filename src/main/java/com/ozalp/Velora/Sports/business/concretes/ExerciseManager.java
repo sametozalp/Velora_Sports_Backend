dc1da66583c8ed4afc0e66e4a6228444a18a17ed
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2025-10-29 15:00:38 +0300

    add exercise create api

--- src/main/java/com/ozalp/Velora/Sports/business/concretes/ExerciseManager.java
+++ src/main/java/com/ozalp/Velora/Sports/business/concretes/ExerciseManager.java
@@ -1,6 +1,9 @@
 package com.ozalp.Velora.Sports.business.concretes;
 
 import com.ozalp.Velora.Sports.business.abstracts.ExerciseService;
+import com.ozalp.Velora.Sports.business.dtos.requests.CreateExerciseRequest;
+import com.ozalp.Velora.Sports.business.dtos.responses.CreateExerciseResponse;
+import com.ozalp.Velora.Sports.business.mappers.ExerciseMapper;
 import com.ozalp.Velora.Sports.entities.concretes.Exercise;
 import com.ozalp.Velora.Sports.repositories.ExerciseRepository;
 import lombok.AllArgsConstructor;
@@ -11,9 +14,16 @@ import org.springframework.stereotype.Service;
 public class ExerciseManager implements ExerciseService {
 
     private final ExerciseRepository repository;
+    private final ExerciseMapper mapper;
 
     @Override
     public Exercise create(Exercise exercise) {
         return repository.save(exercise);
+    }
+
+    @Override
+    public CreateExerciseResponse create(CreateExerciseRequest request) {
+        Exercise exercise = mapper.toEntity(request);
+        return mapper.toResponse(repository.save(exercise));
     }
 }
--- src/main/java/com/ozalp/Velora/Sports/controllers/api/ExerciseController.java
+++ src/main/java/com/ozalp/Velora/Sports/controllers/api/ExerciseController.java
@@ -1,6 +1,12 @@
 package com.ozalp.Velora.Sports.controllers.api;
 
+import com.ozalp.Velora.Sports.business.abstracts.ExerciseService;
+import com.ozalp.Velora.Sports.business.dtos.requests.CreateExerciseRequest;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/exercise")
 @AllArgsConstructor
 public class ExerciseController {
+
+    private final ExerciseService exerciseService;
+
+    @PostMapping("/create")
+    ResponseEntity<?> create(@Valid @RequestBody CreateExerciseRequest request) {
+        return ResponseEntity.ok(exerciseService.create(request));
+    }
 }
