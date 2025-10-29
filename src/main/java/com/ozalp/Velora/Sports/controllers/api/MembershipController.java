f5654975dce897860a3b4d13292dca845ec46d82
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2025-10-29 13:58:34 +0300

    add controllers

---
+++ src/main/java/com/ozalp/Velora/Sports/controllers/api/MembershipController.java
@@ -0,0 +1,9 @@
+package com.ozalp.Velora.Sports.controllers.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/membership")
+public class MembershipController {
+}
