230a09c16aa34e058b586f7066fddf1f24bdf863
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2025-11-08 01:23:14 +0300

    add jwt filter

--- src/main/java/com/ozalp/Velora/Sports/security/JwtService.java
+++ src/main/java/com/ozalp/Velora/Sports/security/JwtService.java
@@ -1,6 +1,7 @@
 package com.ozalp.Velora.Sports.security;
 
 import com.ozalp.Velora.Sports.entities.concretes.User;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 import javax.crypto.SecretKey;
 import java.security.Key;
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class JwtService {
@@ -34,5 +36,33 @@ public class JwtService {
     private SecretKey getSignKey() {
         byte[] bytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(bytes);
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSignKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public String extractEmail(String token) {
+        return getClaims(token).getSubject();
+    }
+
+    public List<String> extractRoles(String token) {
+        return getClaims(token).get("roles", List.class);
+    }
+
+    public Date extractExpiration(String token) {
+        return getClaims(token).getExpiration();
+    }
+
+    public boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
+
+    public Boolean validateToken(String token,String email){
+        return email.equals(extractEmail(token)) && !isTokenExpired(token);
     }
 }
